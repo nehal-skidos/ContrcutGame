6f95ec98c25258e25917e96e5cc5afb54522d0ff
Nehal Ahmad <94435029+nehal-skidos@users.noreply.github.com>
2022-01-13 12:56:23 +0530

    init

---
+++ scripts/project/scriptsInEvents.js
@@ -0,0 +1,18 @@
+function sendData(data){
+// console.log("Data to be sent: "+data);
+// window.location.href = "uniwebview://jsondata?json={'data:'"+data+"}";
+}
+
+
+const scriptsInEvents = {
+
+		async EventSheet1_Event3_Act2(runtime, localVars)
+		{
+			sendData(localVars.data);
+			window.location.href = "uniwebview://jsondata?json={'data:'"+localVars.data+"}";
+		}
+
+};
+
+self.C3.ScriptsInEvents = scriptsInEvents;
+
