317adc079beaa81091273b4781feb3347be7816c
Nehal Ahmad <nehal.ahmad@skidos.com>
2023-10-13 11:48:41 +0530

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -12,14 +12,14 @@ function playVideo(data) {
   } else if (video.canPlayType("application/vnd.apple.mpegurl")) {
     video.src = source;
   }
-  // let playerHeight = $("#skidos_player").height();
-  // let domHeight = $(window).height();
-  // if (domHeight - playerHeight > 0) {
-  //   $(".player-container").css(
-  //     "margin-top",
-  //     (domHeight - playerHeight) / 2 - 50 + "px"
-  //   );
-  // }
+  let playerHeight = $("#skidos_player").height();
+  let domHeight = $(window).height();
+  if (domHeight - playerHeight > 0) {
+    $(".player-container").css(
+      "margin-top",
+      (domHeight - playerHeight) / 2 - 50 + "px"
+    );
+  }
   // Expose player so it can be used from the console
   window.player = player;
   player.play();
